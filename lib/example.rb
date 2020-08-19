105505c037e8d876556ef54fc959e9e6dec30f61
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-08-19 04:22:55 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,6 +1,6 @@
 # Use this space to code along with the readme
 
-def
+def say_hello_world_ten_times
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -12,3 +12,4 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+end
